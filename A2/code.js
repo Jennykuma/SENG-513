4623a85ff036ed0877837339f814e2ec4a321bd1
Jenny <jenny.le@ucalgary.ca>
2018-02-03 21:10:57 -0700

    Stuck on nWords

--- A2/code.js
+++ A2/code.js
@@ -2,9 +2,22 @@
 // this is just a stub for a function you need to implement
 //
 function getStats(txt) {
+
+    let nChars_var = 0
+    nChars_var = txt.length
+
+    let nWords_var = txt.length
+    for (var i = 0; i < nChars_var; i++) {
+        if (((txt.charCodeAt(i) >= 65) && (txt.charCodeAt(i) <= 90)) || ((txt.charCodeAt(i) >= 97) && (txt.charCodeAt(i) <= 122))
+            || ((txt.charCodeAt(i) >= 48) && (txt.charCodeAt(i) <= 57))
+        ){
+            nWords_var -= 1
+        }
+    }
+
     return {
-        nChars: 123,
-        nWords: 22,
+        nChars: nChars_var,
+        nWords: nWords_var,
         nLines: 10,
         nNonEmptyLines: 22,
         averageWordLength: 3.3,
