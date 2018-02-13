8971059af250b18b3780be7277ac71efbf3e4cf4
Jenny <jenny.le@ucalgary.ca>
2018-02-13 14:30:24 -0700

    Did more of the tutorial

--- A3/chat-example/index.js
+++ A3/chat-example/index.js
@@ -1,10 +1,16 @@
 // Express initializes app to be a function handler that you can supply to an HTTP server (as seen in line 3).
 var app = require('express')(); // express is a library
 var http = require('http').Server(app); // http is a library
+var io = require('socket.io')(http); // new instance of io by passing http (http server) obj
 
 // We define a route handler / that gets called when we hit our website home.
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Listen for on the connection event for incoming sockets, log to console
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 // We make the http server listen on port 3000.
